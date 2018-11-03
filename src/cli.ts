e6b640f3a3eba39e85f654b708e5b3dcdc9d2901
johncoffee <github2017@ragelse.dk>
2018-11-03 17:32:47 +0100

    fix wrong address displayed.

--- src/cli.ts
+++ src/cli.ts
@@ -149,22 +149,6 @@ async function info () {
   const contractAddress:string = argv.a || argv.address || require('../ethereum/build/contracts/Sp1.json').networks[networkId].address
   // console.assert(contractAddress)
 
-  enum StateNames {
-    draft = 1,
-    active = 2,
-    terminated = 3,
-  }
-  const stateColours = new Map<StateNames, Function>()
-  stateColours.set(StateNames.draft, yellow)
-  stateColours.set(StateNames.active, greenBright)
-  stateColours.set(StateNames.terminated, blue)
-  const colour = (state:number) => {
-    const func = stateColours.get(state)
-    if (func)
-      return func(StateNames[state])
-    else return StateNames[state]
-  }
-
   console.log(`CONTRACT STATE INFORMATION`)
   console.log()
 
@@ -172,27 +156,42 @@ async function info () {
   await recursiveWalk(contractAddress, web3,`Contract`)
     .catch(err => console.error(red(err)))
 
-  async function recursiveWalk(address:string, web3:any, displayName:string, level:number = 0):Promise<any> {
-    if (address === '0x0000000000000000000000000000000000000000') return Promise.reject('address was 0x')
-
-    const instance = new web3.eth.Contract(require('../ethereum/build/contracts/ICommonState.json').abi as ContractAbi, address)
-
-    const [contractState, numSubContracts] = await Promise.all([
-      <Promise<StateNames>>instance.methods.getState().call(),
-      <Promise<number>>instance.methods.countSubcontracts().call(),
-    ])
-
-    console.log(`${displayName} (at ${shorten(contractAddress)}) is ${colour(parseInt(contractState.toString(), 10))}, has ${numSubContracts} subcontracts`)
-
-    for (let i = 0; i < numSubContracts; i++) {
-      const subContractAddress = await <Promise<string>>instance.methods.getSubcontract(i.toString()).call()
-      await recursiveWalk(subContractAddress, web3, `${' '.repeat(2+level*2)}- subcontract`, level+1)
-    }
-  }
 
   console.log('')
   console.log('OPTIONS')
   console.log(`  - transition contract to active using $ node cli.js 'sign'`)
+}
+enum StateNames {
+  draft = 1,
+  active = 2,
+  terminated = 3,
+}
+const stateColours = new Map<StateNames, Function>()
+stateColours.set(StateNames.draft, yellow)
+stateColours.set(StateNames.active, greenBright)
+stateColours.set(StateNames.terminated, blue)
+const colour = (state:number) => {
+  const func = stateColours.get(state)
+  if (func)
+    return func(StateNames[state])
+  else return StateNames[state]
+}
+async function recursiveWalk(address:string, web3:any, displayName:string, level:number = 0):Promise<any> {
+  if (address === '0x0000000000000000000000000000000000000000') return Promise.reject('address was 0x')
+
+  const instance = new web3.eth.Contract(require('../ethereum/build/contracts/ICommonState.json').abi as ContractAbi, address)
+
+  const [contractState, numSubContracts] = await Promise.all([
+    <Promise<StateNames>>instance.methods.getState().call(),
+    <Promise<number>>instance.methods.countSubcontracts().call(),
+  ])
+
+  console.log(`${displayName} (at ${shorten(address)}) is ${colour(parseInt(contractState.toString(), 10))}, has ${numSubContracts} subcontracts`)
+
+  for (let i = 0; i < numSubContracts; i++) {
+    const subContractAddress = await <Promise<string>>instance.methods.getSubcontract(i.toString()).call()
+    await recursiveWalk(subContractAddress, web3, `${' '.repeat(2+level*2)}- subcontract`, level+1)
+  }
 }
 
 async function add () {
