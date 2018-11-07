ff58ccc8a24b510925142502e5f17e2ece072642
johncoffee <github2017@ragelse.dk>
2018-11-07 11:58:04 +0100

    debug

--- ethereum/migrations/3_sp1.js
+++ ethereum/migrations/3_sp1.js
@@ -14,12 +14,12 @@ module.exports = function(deployer, network, accounts) {
     .then(() => deployer.deploy(Sp1, municipality, serviceProvider) )
     .then(instance => {
       // sp1Instance.
-      console.log("  SP _1_  " + instance.address)
+      console.log("  TEST SP _1_  " + instance.address)
       sp1InstanceParent = instance
       return deployer.deploy(Sp1, serviceProvider, school)
     })
     .then(instance => {
-      console.log("  SP _1_2 " + instance.address)
+      console.log("  TEST SP _1_2 " + instance.address)
       return sp1InstanceParent.add(instance.address, {from: serviceProvider})
     })
 
