19dd6e9be64dfd61ceeda6c876f691fccdffee70
johncoffee <github2017@ragelse.dk>
2018-11-09 00:27:19 +0100

    more example contracts

---
+++ ethereum/contracts/MultisigTerminate.sol
@@ -0,0 +1,32 @@
+pragma solidity ^0.4.0;
+
+import './owned.sol';
+import "./ICommonState.sol";
+import "./CommonStates.sol";
+
+contract MultisigTerminate is ICommonState, CommonStates, Owned {
+    uint state = ACTIVE;
+
+    address public paymentAddress;
+    bytes32 public hash;
+    uint public totalPrice;
+
+    constructor(address _owner, address _paymentAddress) Owned(_owner) public {
+        paymentAddress = _paymentAddress;
+    }
+
+    function terminate() external ownerOnly {
+        require(paymentAddress.balance >= totalPrice);
+        state = TERMINATED;
+    }
+
+    function getState() external constant returns(uint) {
+        return state;
+    }
+    function countSubcontracts() external constant returns(uint) {
+        return 0;
+    }
+    function getSubcontract(uint _index) external constant returns(address) {
+        return 0;
+    }
+}
